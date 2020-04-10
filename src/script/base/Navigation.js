04b59b55253bfae232b2de751ed520ff28327e4f
AndreyAmelchenia <60568794+AndreyAmelchenia@users.noreply.github.com>
2020-04-10 22:41:52 +0300

    feat: add _check.scss and element checkbox

--- src/script/base/Navigation.js
+++ src/script/base/Navigation.js
@@ -8,26 +8,22 @@ class Navigation {
 
   init() {
     this.navTrigger.addEventListener('click', () => {
-      if (!this.navTrigger.classList.contains('is-active')) {
+      if (!this.navTrigger.classList.contains('is-active') && !this.nav.classList.contains('is-active')) {
         this.navTrigger.classList.add('is-active');
-        if (!this.nav.classList.contains('is-active')) {
-          this.nav.classList.add('is-active');
-          this.nav.addEventListener('transitionend', (e) => {
-            if (e.propertyName === 'width' && this.navTrigger.classList.contains('is-active')) {
-              this.navContent.classList.add('is-active');
-            }
-          });
-        }
+        this.nav.classList.add('is-active');
+        this.nav.addEventListener('transitionend', (event) => {
+          if ((event.propertyName === 'width') && this.navTrigger.classList.contains('is-active')) {
+            this.navContent.classList.add('is-active');
+          }
+        });
       } else {
         this.navTrigger.classList.remove('is-active');
-        if (this.navContent.classList.contains('is-active')) {
-          this.navContent.classList.remove('is-active');
-          this.navContent.addEventListener('transitionend', (event) => {
-            if (event.propertyName === 'opacity' && !this.navTrigger.classList.contains('is-active')) {
-              this.nav.classList.remove('is-active');
-            }
-          });
-        }
+        this.navContent.classList.remove('is-active');
+        this.navContent.addEventListener('transitionend', (event) => {
+          if (event.propertyName === 'opacity' && !this.navTrigger.classList.contains('is-active')) {
+            this.nav.classList.remove('is-active');
+          }
+        });
       }
     });
   }
